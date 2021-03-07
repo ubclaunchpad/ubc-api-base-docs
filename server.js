0d599c86dc2a8ad7982f38554676831d8da40c00
michelleteh <nmteh17@students.cs.ubc.ca>
2021-03-07 11:48:37 +0700

    First commit

---
+++ server.js
@@ -0,0 +1,54 @@
+var express = require("express"),
+  bodyParser = require("body-parser"),
+  swaggerJsdoc = require("swagger-jsdoc"),
+  swaggerUi = require("swagger-ui-express");
+
+const app = express();
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+
+app.use("/books", require("./routes/books"));
+
+const PORT = process.env.PORT || 3000;
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "LogRocket Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "LogRocket",
+        url: "https://logrocket.com",
+        email: "info@email.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/books",
+      },
+    ],
+  },
+  apis: ["./routes/books.js"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+  "/tester",
+  swaggerUi.serve,
+  swaggerUi.setup(specs)
+);
+
+app.listen(PORT);
+
+console.debug("Server listening on port: " + PORT);
